9e9e7903d138c9d39fe596a85a2657cfac89b085
Sachin Dewra <sachin.dewra@gmail.com>
2023-07-22 13:44:48 +0530

    combining reduces

---
+++ src/redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from "../shared/Promotions";
+
+export const Promostions = (state = PROMOTIONS,action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
